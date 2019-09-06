962ec0b074ac92511075f7cdad0f20f1a2cdd9f3
gugu <lar542@naver.com>
2019-09-06 23:57:43 +0900

    모임 개설에 카카오맵 추가

--- src/test/java/com/tips/webservice/event/EventRepositoryTest.java
+++ src/test/java/com/tips/webservice/event/EventRepositoryTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.tips.webservice.event.Event;
 import com.tips.webservice.event.EventRepository;
 
 @RunWith(SpringRunner.class)
